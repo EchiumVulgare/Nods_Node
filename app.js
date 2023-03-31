464e443637490546453e777d27f0716ae59e762f
Alex P <881507484pl@gmail.com>
2023-03-31 15:46:49 +0200

    edition add

---
+++ app.js
@@ -0,0 +1,57 @@
+console.log('Starting app.js')
+const fs = require('fs');
+const os = require('os');
+var user = os.userInfo();
+const notes = require('./notes');
+let res = notes.addNote();
+const yargs = require('yargs');
+
+const argv = yargs.argv;
+let command = process.argv[2];
+// console.log("Komenda:",command);
+console.log("Proces:",process.argv);
+console.log("Yargs",argv);
+
+
+if (command === 'add'){
+    console.log('Dodawanie nowej notatki');
+    let note = notes.addNote(argv.title, argv.body);
+    if (note){
+        console.log('Notatka dodana!');
+    } else {
+        console.log('Notatka o takiej nazwie już istnieje!');
+    }
+}
+    else if (command === 'list'){
+        notes.getAll();
+    }
+    else if (command === 'read'){
+        notes.getNote(argv.title);
+    }
+    else if (command === 'remove'){
+        notes.removeNote(argv.title);
+    }
+    else {
+        console.log('Nie rozpoznana komenda!');
+    }
+
+    
+
+
+// console.log(res);
+
+// fs.appendFileSync('powitanie2.txt', `Witaj ${user.username} !Masz ${notes.age}.`);
+
+
+// fs.appendFile('powitanie.txt','Hello JS!'); //depricated (max. v.10)
+
+// fs.appendFileSync('powitanie.txt','Hello JS ' + user.username + "!");
+
+
+// fs.appendFile('powitanie.txt','Hello JS ponownie!',function (err){
+//     if (err){
+//         console.log('Problem z dostępem do pliku!')
+//     }
+// });
+
+// console.log(user);
---
+++ notes.js
@@ -0,0 +1,54 @@
+console.log("Starting notes.js");
+// console.log(module);
+// module.exports.age = 25;
+// module.exports.addNote = function(){
+
+// }
+
+const fs = require("fs");
+
+let fetchNotes = () => {
+  try {
+    let notesString = fs.readFileSync("notes-data.json");
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+let saveNotes = (notes) => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
+let addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body,
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+let getAll = () => {
+  console.log("Pobranie listy notatek...");
+};
+
+let getNote = (title) => {
+  console.log("Pobieram notatkę: ", title);
+};
+
+let removeNote = (title) => {
+  console.log("Kasowanie notatki: ", title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
